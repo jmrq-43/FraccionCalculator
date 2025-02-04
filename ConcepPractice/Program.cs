487d51691c0e39450f0ddeb6f597ee73854cfd26
jmrq-43 <rojasquinteroj26@gmail.com>
2025-02-03 23:02:27 -0500

    Implemeting Fraccion class

--- ConcepPractice/Program.cs
+++ ConcepPractice/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConcepPractice.calculator;
+
+namespace ConcepPractice;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Fraccion fraccio1 = new Fraccion(2, 3);
+        Fraccion fraccio2 = new Fraccion(4, 5);
+
+        Fraccion suma = fraccio1.SumFraction(fraccio2);
+
+        Console.WriteLine(suma);
+    }
+}
+
